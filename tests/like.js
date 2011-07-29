eb8ec4a1021b663cb772d39fcdb7f8072d6b6e4e
Justin Bishop <jubishop@gmail.com>
2011-07-28 18:50:02 -0700

    break out like utility in preparation for more like tests.

--- tests/like.js
+++ tests/like.js
@@ -3,20 +3,31 @@ var fb = require('./infra/core.js')
   , soda = require('./infra/soda.js')
   , settings = require('./../settings.js')
 
-api.createTestUser(function(er, test_user) {
-  soda.sodaTest(exports, 'like and unlike with edge events', function(browser) {
-    var url = '/saved/dfba30ac7d85862f1da8c9e2c5f20228'
-    return browser
-      .and(fb.runLoggedInExample({
-        url: url, email: test_user.email, pass: test_user.password }))
-      .and(fb.runInIFramePlugin(function(browser) {
-        return browser.click('css=.like_button_no_like')
-      }))
-      .and(fb.waitAssertTextPresent('You liked http://fbrell.com/'))
-      .and(fb.runExample({ url: url }))
-      .and(fb.runInIFramePlugin(function(browser) {
-        return browser.click('css=.like_button_like .tombstone_cross')
-      }))
-      .and(fb.waitAssertTextPresent('You unliked http://fbrell.com/'))
-  })
+var edge_event_url = '/saved/dfba30ac7d85862f1da8c9e2c5f20228'
+
+/**
+ * Tests
+ */
+soda.sodaTest(exports, 'like/unlike with edge events', function(browser) {
+  return browser
+    .and(fb.runLoggedInExample({ url: edge_event_url}))
+    .and(clickLike(browser))
+    .and(fb.waitAssertTextPresent('You liked http://fbrell.com/'))
+    .and(clickUnlike(browser))
+    .and(fb.waitAssertTextPresent('You unliked http://fbrell.com/'))
 })
+
+/**
+ * Utility
+ */
+function clickLike(browser) {
+  return fb.runInIFramePlugin(function(browser) {
+    return browser.click('css=.like_button_no_like')
+  })
+}
+
+function clickUnlike(browser) {
+  return fb.runInIFramePlugin(function(browser) {
+    return browser.click('css=.like_button_like .tombstone_cross')
+  })
+}
