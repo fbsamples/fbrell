85af8bbbb51fe6ede23c5b71b48eab132114dc24
Naitik Shah <n@daaku.org>
2012-02-27 15:50:39 -0800

    start working on soda tests again, port from expresso to mocha

--- tests/api.js
+++
@@ -1,7 +0,0 @@
-fb = require('./infra.js')
-
-fb.sodaTest(exports, 'user info via API', function(browser) {
-  return browser
-    .and(fb.runLoggedInExample({ url: '/fb.api/user-info?autoRun' }))
-    .and(fb.waitAssertTextPresent('first_name'))
-})
