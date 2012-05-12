2b2beb22dfdeb83b695bc56859f1dd60e4d8492a
Naitik Shah <n@daaku.org>
2012-05-12 00:07:03 -0700

    another bump

--- context/viewcontext/context.go
+++ context/viewcontext/context.go
@@ -22,7 +22,7 @@ func Info(w http.ResponseWriter, r *http.Request) {
 		"canvasURL":  context.CanvasURL(),
 		"channelURL": context.ChannelURL(),
 		"sdkURL":     context.SdkURL(),
-		"version":    "3.0.5",
+		"version":    "3.0.6",
 	}
 	json.NewEncoder(w).Encode(info)
 }
