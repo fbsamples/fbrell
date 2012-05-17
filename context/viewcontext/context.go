2b2b2ab8c3aae5a72d8eacd01aa123e591bffad7
Naitik Shah <n@daaku.org>
2012-05-16 18:21:16 -0700

    better /info/

--- context/viewcontext/context.go
+++ context/viewcontext/context.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"github.com/nshah/rell/context"
 	"github.com/nshah/rell/view"
+	"log"
 	"net/http"
 )
 
@@ -17,11 +18,24 @@ func Info(w http.ResponseWriter, r *http.Request) {
 	}
 	w.Header().Set("Content-Type", "application/json; charset=utf-8")
 	info := map[string]interface{}{
+		"request": map[string]interface{}{
+			"method": r.Method,
+			"form":   r.Form,
+			"url": map[string]interface{}{
+				"path":  r.URL.Path,
+				"query": r.URL.RawQuery,
+			},
+		},
 		"context":    context,
 		"pageTabURL": context.PageTabURL("/"),
 		"canvasURL":  context.CanvasURL("/"),
 		"sdkURL":     context.SdkURL(),
 		"version":    "3.0.8",
 	}
-	json.NewEncoder(w).Encode(info)
+	out, err := json.MarshalIndent(info, "", "  ")
+	if err != nil {
+		log.Printf("Error json.MarshalIndent for /info/: %s", err)
+	}
+	w.Write(out)
+	w.Write([]byte("\n"))
 }
