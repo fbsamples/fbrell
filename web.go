ac2e857f3abdba8d4accdfc9f6b3b18e7ce3b597
Naitik Shah <n@daaku.org>
2012-10-05 12:15:26 -0700

    still need go.stats.SetBackend for other stuff

--- web.go
+++ web.go
@@ -13,11 +13,13 @@ import (
 	"github.com/daaku/go.httpstats"
 	"github.com/daaku/go.signedrequest/appdata"
 	"github.com/daaku/go.static"
+	"github.com/daaku/go.stats"
 	"github.com/daaku/go.viewvar"
 	"github.com/daaku/rell/context/viewcontext"
 	"github.com/daaku/rell/examples/viewexamples"
 	"github.com/daaku/rell/oauth"
 	"github.com/daaku/rell/og/viewog"
+	"github.com/daaku/rell/service"
 	"log"
 	"net/http"
 	"net/http/pprof"
@@ -49,6 +51,7 @@ func main() {
 	flag.Usage = flagconfig.Usage
 	flag.Parse()
 	flagconfig.Parse()
+	stats.SetBackend(service.Stats)
 	err := gracehttp.Serve(
 		gracehttp.Handler{*mainAddress, mainHandler()},
 		gracehttp.Handler{*adminAddress, adminHandler()},
